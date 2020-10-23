55c0d5004a648afb6e3c77ef958e80e0099a3f9c
Mykola Svystun <nickolasmv@gmail.com>
2020-10-23 14:44:20 +0300

    fix minify

--- src/messenger.js
+++ src/messenger.js
@@ -4,28 +4,30 @@
 /* eslint-disable no-undef */
 'use strict';
 
-const mongoose = require('mongoose');
 const util = require('util');
 const EventEmitter = require('events').EventEmitter;
-const MessageSchema = new mongoose.Schema({
-  channel: String,
-  createdAt: { type: Date, expires: 3600, default: Date.now },
-  message: mongoose.Schema.Types.Mixed,
-}, {
-  capped: {
-    size: 1024 * 16 * 25, // in bytes
-    autoIndexId: true
-  }
-});
 
-const Message = mongoose.models['PubSubMessage'] || mongoose.model('PubSubMessage', MessageSchema);
 
-function Messenger(options){
+
+function Messenger(options) {
   EventEmitter.apply(this, arguments);
   var o = options || {};
   if (o.mongooseOptions) {
     this.mongooseOptions = o.mongooseOptions;
   }
+  this.mongoose = o.mongoose || require('mongoose');
+
+  const MessageSchema = new mongoose.Schema({
+    channel: String,
+    createdAt: { type: Date, expires: 3600, default: Date.now },
+    message: mongoose.Schema.Types.Mixed,
+  }, {
+    capped: {
+      size: 1024 * 16 * 25, // in bytes
+      autoIndexId: true
+    }
+  });
+  this.Message = mongoose.models['PubSubMessage'] || mongoose.model('PubSubMessage', MessageSchema);
   this.subscribed = {};
   this.lastMessageTimestamp = null;
   this.startingMessageTimestamp = new Date();
@@ -34,12 +36,12 @@ function Messenger(options){
 
 util.inherits(Messenger, EventEmitter);
 
-Messenger.prototype.send = function (channel, msg, callback){
-  var cb = function noop(){};
-  if(typeof callback === 'function'){
+Messenger.prototype.send = function (channel, msg, callback) {
+  var cb = function noop() { };
+  if (typeof callback === 'function') {
     cb = callback;
   }
-  var message = new Message({
+  var message = new this.Message({
     channel: channel,
     message: msg
   });
@@ -48,21 +50,21 @@ Messenger.prototype.send = function (channel, msg, callback){
 
 Messenger.prototype.connect = function (callback) {
   var self = this;
-  if (mongoose.connection.readyState == 0 && self.mongooseOptions ) {
-    mongoose.connect(self.mongooseOptions.url,self.mongooseOptions.options)
+  if (this.mongoose.connection.readyState == 0 && self.mongooseOptions) {
+    this.mongoose.connect(self.mongooseOptions.url, self.mongooseOptions.options)
   }
-  var stream = Message.watch();// .on('change', change => console.log(change));
+  var stream = this.Message.watch();// .on('change', change => console.log(change));
 
   stream.on('change', function data(doc) {
     const { fullDocument } = doc;
     self.lastMessageTimestamp = fullDocument.createdAt;
-    if(self.subscribed[fullDocument.channel]){
+    if (self.subscribed[fullDocument.channel]) {
       self.emit(fullDocument.channel, fullDocument.message);
-    } 
+    }
   });
 
   // reconnect on error
-  stream.on('error', function streamError(){
+  stream.on('error', function streamError() {
     stream.destroy();
     self.connect();
   });
@@ -72,16 +74,16 @@ Messenger.prototype.connect = function (callback) {
     self.connect();
   });
 
-  if(callback) callback();
+  if (callback) callback();
 };
 
 Messenger.prototype.subscribe = function (channel, bool) {
   var self = this;
-  if(channel && bool){
+  if (channel && bool) {
     self.subscribed[channel] = bool;
     return;
   }
-  if(channel && self.subscribed[channel]){
+  if (channel && self.subscribed[channel]) {
     delete self.subscribed[channel];
   }
 };
